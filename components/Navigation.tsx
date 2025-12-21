3e2ee627fe5c341ee198722f52ae698a53974d30
piechstudio-create <piechstudio@gmail.com>
2025-12-21 23:23:19 +0100

    feat: Update logo to new version (Logo-www-2)

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 const Navigation = () => {
   return (
     <nav style={{ marginBottom: '2rem', borderBottom: '2px solid #ddd', padding: '1rem 0' }}>
-      <Image src="https://panakompaniator.pl/wp-content/uploads/2025/10/cropped-Logotyp-PA-Avatar-transparent-1.png" alt="Pan Akompaniator" width={50} height={50} className="mr-4" />
+      <Image src="https://panakompaniator.pl/wp-content/uploads/2025/10/cropped-Logo-www-2.png" alt="Pan Akompaniator" width={50} height={50} className="mr-4" />
       <ul style={{ listStyle: 'none', padding: 0, display: 'flex', gap: '2rem' }}>
         <li>
           <Link href="/kursy" style={{ fontSize: '1.1rem', color: '#666' }}>
