694d9d295a99589ca22fea0ce4c04f259f3baf36
piechstudio-create <piechstudio@gmail.com>
2025-12-21 23:19:13 +0100

    Update Navigation.tsx

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,7 +1,9 @@
 import Link from 'next/link';
+import Image from 'next/image';
 const Navigation = () => {
   return (
     <nav style={{ marginBottom: '2rem', borderBottom: '2px solid #ddd', padding: '1rem 0' }}>
+      <Image src="https://panakompaniator.pl/wp-content/uploads/2025/10/cropped-Logotyp-PA-Avatar-transparent-1.png" alt="Pan Akompaniator" width={50} height={50} className="mr-4" />
       <ul style={{ listStyle: 'none', padding: 0, display: 'flex', gap: '2rem' }}>
         <li>
           <Link href="/kursy" style={{ fontSize: '1.1rem', color: '#666' }}>
