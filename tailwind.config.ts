8e5dedcf0a4628c8fc793d01bb8bb2c8c86f4735
piechstudio-create <piechstudio@gmail.com>
2025-12-22 15:24:12 +0100

    fix: Add animation delay utilities to Tailwind config for blob animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -37,6 +37,8 @@ const config: Config = {
       },
       animation: {
         'blob': 'blob 7s infinite',
+                    'animation-delay-2000': 'animation-delay: 2s',
+                    'animation-delay-4000': 'animation-delay: 4s',
       },
       keyframes: {
         blob: {
